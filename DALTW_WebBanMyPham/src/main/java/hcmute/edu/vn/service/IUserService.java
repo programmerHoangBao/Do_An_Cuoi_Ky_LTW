302189a37fa175838849d16dd1b38ef3d1b1127e
PhamDangKhoi2803 <22110357@student.hcmute.edu.vn>
2024-12-04 21:31:01 +0700

    Chỉnh sửa UserService

--- DALTW_WebBanMyPham/src/main/java/hcmute/edu/vn/service/IUserService.java
+++ DALTW_WebBanMyPham/src/main/java/hcmute/edu/vn/service/IUserService.java
@@ -6,4 +6,5 @@ public interface IUserService {
     User findByUsername(String username);
     User updateRoleVentor(User user);
     void updateUser(User user);
+    User checkLogin(String email, String pass);
 }
