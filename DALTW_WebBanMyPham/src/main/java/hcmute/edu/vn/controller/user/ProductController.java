c4ec5ac83d32863251fccdd6b9b4de2c2f9f4fd6
Hùng Nguyễn <vanhung211104@gmail.com>
2024-12-05 18:41:18 +0700

    Sửa bảng FavoriteShop thành HistoryProduct

--- DALTW_WebBanMyPham/src/main/java/hcmute/edu/vn/controller/user/ProductController.java
+++ DALTW_WebBanMyPham/src/main/java/hcmute/edu/vn/controller/user/ProductController.java
@@ -8,20 +8,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ProductController {
 
     @Autowired
     ProductService productService;
-    @Autowired
-    ProductColorService productColorService;
+
 
     @GetMapping("/product/{id}")
     public String productDetail(@PathVariable("id") Integer id, ModelMap model) {
         Product product = productService.findProductById(id);
-        product.setProductColors(productColorService.findProductColorByIdProduct(id));
         model.addAttribute("product", product);
         return "user/product-detail";
+    }
+
+    @GetMapping("/followshop/{idShop}&{idUser}")
+    @ResponseBody
+    public String followShop(@PathVariable("idShop") Integer idShop, @PathVariable("idUser") Integer idUser) {
+        // Logic xử lý khi người dùng theo dõi shop
+        // Ví dụ: lưu vào cơ sở dữ liệu
+
+        // Trả về một thông báo hoặc dữ liệu thành công dưới dạng JSON (nếu cần)
+        return "{\"status\":\"success\", \"message\":\"Followed the shop successfully\"}";
     }
 }
