4cdd5047075bb9c06669b6e096d3a31fff1f046c
Bao <universitynhb@gmail.com>
2024-12-07 12:31:58 +0700

    Hoàn thiện chức năng tìm kiếm version 1.1.0

--- DALTW_WebBanMyPham/src/main/java/hcmute/edu/vn/service/IProductService.java
+++ DALTW_WebBanMyPham/src/main/java/hcmute/edu/vn/service/IProductService.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 
 import hcmute.edu.vn.entity.Product;
 
 
 public interface IProductService {
 	List<Product> findAll();
-	List<Product> findProductByNameContaining(String nameProduct);
+	Page<Product> findProductByNameContaining(String name, Pageable pageable);
 	Page<Product> findProductPageable(Pageable pageable);
 	Product findProductById(Integer id);
 	Product saveProduct(Product product);
